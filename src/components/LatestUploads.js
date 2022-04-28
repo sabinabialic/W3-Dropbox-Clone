b86065bcfd8d60c153626e17cdcd5945f76b448b
Sabina <sabina.bialic@gmail.com>
2022-04-28 09:19:58 -0400

    Latest Uploads Section

--- src/components/LatestUploads.js
+++ src/components/LatestUploads.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class LatestUploads extends Component {
     render() {
       return (
-        <div>
-            
+        <div className="bg-[#181918] m-5 flex flex-1">
+          <div className="flex flex-col items-center">
+              <h1 className='text-white text-center'>Latest Uploads</h1>
+          </div>
         </div>
       );
     }
